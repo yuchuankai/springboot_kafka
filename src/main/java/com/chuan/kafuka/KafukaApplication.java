c453ac90d85e73578ad064a4b7338ef7229f065a
yuchuankai <yuchuankai@bjsasc.com>
2022-09-23 14:35:51 +0800

    init

---
+++ src/main/java/com/chuan/kafuka/KafukaApplication.java
@@ -0,0 +1,13 @@
+package com.chuan.kafuka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafukaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafukaApplication.class, args);
+    }
+
+}
